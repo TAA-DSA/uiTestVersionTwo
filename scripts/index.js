f7f3857c42cb007978548ba29dbcf8a42170a4cd
Tasdeed <ayonaziz@gmail.com>
2023-09-30 19:48:27 -0400

    version-2 updated relative paths

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 const signUpBtn = () => {
-  window.location.href = "../pages/login.html";
+  window.location.href = "./pages/login.html";
 };
 
 const loginBtn = () => {
-  window.location.href = "../pages/signUp.html";
+  window.location.href = "./pages/signUp.html";
 };
